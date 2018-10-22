ea6fee88680f7345dfbf76b163fc066ba1d73f3e
Cam Powell <Cam@Users-MacBook-Air.local>
2018-10-22 12:00:31 +0100

    beep.py complete

---
+++ Beep.py
@@ -0,0 +1,33 @@
+import random
+#import winsound
+
+def playGame():
+    print("Ok", name,", I will sound the speaker beep at a particular frequency and you must estimate the frequency.")
+
+    playFrequency = random.randint(37, 8000)
+    #winsound.Beep(playFrequency, 1)
+    frequency = input("What frequency do you estimate that sound to have been? ")
+    if frequency == playFrequency:
+        input("You are correct!")
+    else:
+        print("Hard luck, the frequency was", playFrequency)
+        input()
+
+
+
+name = input("Please enter your name: ")
+i = 1
+while i <= 10:
+    response = input("Do you want to take the estimate the frequency challenge? Enter y or n: ")
+    if response == "y":
+        playGame()
+        i = i + 1
+    else:
+        quit()
+
+input("You have exceeded your estimate limit!")
+quit()
+
+
+
+
